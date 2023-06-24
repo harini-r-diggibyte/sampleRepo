478c17f3a684c2c4beace0afc78d8e4ae315be54
harini GitHub <harini.r@diggibyte.com>
2023-06-24 18:27:02 +0530

    2nd Commit with df , sum func.

--- sampleRepo/sample.py
+++ sampleRepo/sample.py
@@ -1,14 +1,20 @@
 from pyspark.sql.types import *
 from pyspark.sql.functions import *
 from pyspark.sql import SparkSession
-spark=SparkSession.builder.getOrCreate()
-data=[("aaa",23),("bbb",34)]
-schema=StructType([StructField("name",StringType(),True),
-                   StructField("age",IntegerType(),True)])
-df=spark.createDataFrame(data=data,schema=schema)
+
+spark = SparkSession.builder.getOrCreate()
+data = [("aaa", 23), ("bbb", 34)]
+schema = StructType([StructField("name", StringType(), True),
+                     StructField("age", IntegerType(), True)])
+df = spark.createDataFrame(data=data, schema=schema)
 df.show()
-def sum(a,b):
-    return a+b
-n1,n2=map(int(),input().split())
-result=sum(n1,n2)
+
+
+def sumofnum(a, b):
+    return a + b
+
+
+# n1,n2=map(int,input().split())
+
+result = sumofnum(5, 10)
 print(result)
