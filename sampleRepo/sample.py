4b14a17bb9ce4ab91b03dfd98daad06f746fef6c
harini GitHub <harini.r@diggibyte.com>
2023-06-24 15:07:43 +0530

    2nd Commit with df , sum func.

--- sampleRepo/sample.py
+++ sampleRepo/sample.py
@@ -2,8 +2,13 @@ from pyspark.sql.types import *
 from pyspark.sql.functions import *
 from pyspark.sql import SparkSession
 spark=SparkSession.builder.getOrCreate()
-
+data=[("aaa",23),("bbb",34)]
+schema=StructType([StructField("name",StringType(),True),
+                   StructField("age",IntegerType(),True)])
+df=spark.createDataFrame(data=data,schema=schema)
+df.show()
 def sum(a,b):
     return a+b
 n1,n2=map(int(),input().split())
-print(sum(n1,n2))
+result=sum(n1,n2)
+print(result)
