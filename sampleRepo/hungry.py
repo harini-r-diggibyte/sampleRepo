198703106da3117dbd1be6f91a76da16faffc149
harini GitHub <harini.r@diggibyte.com>
2023-06-24 19:58:23 +0530

    final check to see push works from gitbash

--- sampleRepo/hungry.py
+++ sampleRepo/hungry.py
@@ -1,5 +1,5 @@
 print("hungry")
 print("if you are hungry, then have some kachori")
 print("if you are not hungry wait then")
-print("then")
+print("then checking for push works from gitbash")
 print("thank you")
